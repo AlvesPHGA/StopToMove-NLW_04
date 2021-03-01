b8d9fc590f50bd09a4e5b37152471f1f79842705
alvesphga <pedrohenriquegomesalves@gmail.com>
2021-03-01 08:32:35 -0300

    Update perfil.tsx

--- src/components/Perfil.tsx
+++ src/components/Perfil.tsx
@@ -6,9 +6,9 @@ export function Profile(){
     const {level}= useContext(challengesContext);
     return(
         <div className={styles.perfilContainer}>
-            <img src="https://avatars.githubusercontent.com/u/35383608?s=400&u=121a93a6e6482e42b1991508180fffb7720f0306&v=4" alt="Foto do usuario"/>
+            <img src="/yoda.jpg" alt="Foto do usuario"/>
             <div>
-                <strong>Pedro Henrique</strong>
+                <strong>User Name</strong>
                 <p>
                     <img src="icons/level.svg" alt=""/>
                     level {level}
